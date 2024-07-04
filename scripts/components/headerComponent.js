a97e110aae07768436320a2803289954dfb68353
Dowee <Dowee24@gmail.com>
2024-07-03 23:00:13 -0400

    Used Vue to replicate Headers updated Resume, fixed index portrait extending past border

--- scripts/components/headerComponent.js
+++ scripts/components/headerComponent.js
@@ -1,3 +1,10 @@
 app.component('headercomponent', {
-    template: ''
+    template: `<section id="socials">
+            <div class="sm_icon">
+                <a href="https://www.linkedin.com/in/anton-maynard-24a246127/" target="blank"><img src="images/logos/LinkedIn.png"></a>
+                <a href="https://github.com/yourusername" target="_blank"> <img src="images/logos/github-mark-white.svg" alt="GitHub"></a>
+            </div>
+        </section>
+        <navcomponent></navcomponent>
+        `
 })
--- scripts/components/navComponent.js
+++ scripts/components/navComponent.js
@@ -4,7 +4,7 @@ app.component('navcomponent', {
         <li><a href="index.html">Home</a></li>
         <li><a href="resume.html">Resume</a></li>
         <li><a href="about.html">Personal Time</a></li>
-        <li class="dropdown">Accomplishments
+        <li class="dropdown">Projects
             <div class="dropdown-content">
                 <a href="PTW.html">Professional And Technical Writing</a>
                 <a href="CS.html">Computer Science</a></div>
